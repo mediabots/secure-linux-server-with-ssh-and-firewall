5756706dc418ae881b56ce36dc09788e9b708902
mediabots <mediabots@mail.ru>
2024-02-28 16:08:48 +0530

    [Feature] create a user with sudo and config that user; configure a secure ssh; install firewalld and config it to allow ssh,http,https

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+ export HISTCONTROL=ignoredups:ignorespace
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
 
 tmp_file_path=/tmp/.script #$HOME/.script
 
@@ -45,7 +51,7 @@ then
 	ssh-keygen -t ed25519 -C $filename -f $HOME/.ssh/$filename -P "${PHRASE}"
 	echo "========================================================"
 	echo "your passphrase for file - "$filename "is: "$PHRASE
-	echo "IMPORTANT: please note it down in your non-digital notebook. This value in non-recoverable"
+	echo "${GREEN}IMPORTANT: please note it down in your non-digital notebook. ${NC}This value is non-recoverable"
 	cat $HOME/.ssh/$filename.pub
 	echo "above is the public key of your SSH key pair. You can share it with your Hosting/Cloud provider"
 	exit 0;
@@ -105,9 +111,111 @@ then
 	fi
 fi
 
-if [ "$t_out" == "remote" ];
-then
+if [ "$t_out" == "remote" ]; then
 	echo $t_out;
+
+	# setting default values if ENV were not set
+	if [ -z $USERID ]; then
+		USERID='ubuntu'
+	fi
+	if [ -z $PASSWORD ]; then
+		 PASSWORD=$(tr </dev/urandom -dc A-Za-z0-9*%^+~ | head -c6)$(tr </dev/urandom -dc 0-9 | head -c1)$(tr </dev/urandom -dc *%^+~ | head -c1)
+	fi
+	if [ -z $PORT ]; then
+		if [ ! -z $3 ]; then
+			number=$3
+			if [ -z "${number//[0-9]}" ]; then
+				if [ -n "$number" ]; then
+					PORT=$number
+				fi
+			else
+				PORT=60022
+			fi
+		else
+			PORT=60022
+		fi
+	fi
+
+	# if new user is already exist into the Remote machine then just add that user into the sudo group, else create new user with sudo privilege
+	if [ `ls /home | grep $USERID | wc -l` = 1 ]; then 
+		sudo usermod -aG sudo $USERID; 
+	else 
+		sudo useradd --create-home --user-group --groups sudo --shell /bin/bash $USERID; 
+	fi
+
+	# confirm created user groups
+	groups $USERID
+
+	# set new user password
+	 echo -e "$PASSWORD\n$PASSWORD" | (sudo passwd $USERID)
+
+	# get current/old user directory 
+	USERDIR=`eval echo ~$USER`; 
+
+	# pre-check SSH Auth requirements
+	if [ ! -f $USERDIR/.ssh/authorized_keys ]; then
+		echo "Erro: looks like this remote machine has not setup for SSH authentication"
+		echo "=> to know how to setup SSH authentication, please refer to our Script documentation"
+		exit 1;
+	fi
+	if [ $(cat $USERDIR/.ssh/authorized_keys | wc -l) -eq 0 ]; then 
+		echo "Erro: no public key has been added to this remote machine to allow SSH authentication from out_side/other_side"
+		echo "=> to know how to add a public key, please refer to our Script documentation"
+		exit 1;
+	fi
+
+	# make .ssh folder for new user and set correct permission & ownership for that folder and its files. Then copy current/old user SSH keys to new user
+	sudo mkdir -p /home/$USERID/.ssh
+	sudo chmod 700 /home/$USERID/.ssh
+	sudo cp -r $USERDIR/.ssh/* /home/$USERID/.ssh/
+	sudo chmod 600 /home/$USERID/.ssh/authorized_keys
+	sudo chown $USERID:$USERID /home/$USERID/.ssh -R
+
+	# change default ssh port, disable password-based login, disable any kind of login for root; allow ssh-based authentication, and allow ssh-based port forwarding
+	sudo sed -i "s/#\?Port.*$/Port $PORT/g" /etc/ssh/sshd_config
+	sudo sed -i 's/#\?PermitRootLogin.*$/PermitRootLogin no/g' /etc/ssh/sshd_config
+	sudo sed -i 's/#\?PasswordAuthentication.*$/PasswordAuthentication no/g' /etc/ssh/sshd_config
+	sudo sed -i 's/#\?PermitEmptyPasswords.*$/#PermitEmptyPasswords no/g' /etc/ssh/sshd_config
+	sudo sed -i 's/#\?PubkeyAuthentication.*$/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+	sudo bash -c ' echo "GatewayPorts yes" >> /etc/ssh/sshd_config'
+
+	# restart ssh server to make above changes
+	sudo systemctl restart sshd
+
+	# install feature rich Firewall
+	sudo systemctl stop ufw
+	sudo systemctl disable --now ufw
+	sleep 3
+	sudo apt remove --yes --purge ufw
+	sudo apt update && sudo -E apt -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" -qq -y install firewalld --allow-change-held-packages
+
+	# confirm Firewall state
+	sudo firewall-cmd --state
+
+	# replace ssh default port to the new ssh port so Firewall will whitelist that new port for incoming connection  
+	sudo sed -i "s/port=\".*\"/port=\"$PORT\"/g" /usr/lib/firewalld/services/ssh.xml
+	
+	# reload Firewall to take the effect of the above config changes
+	sudo firewall-cmd --reload
+
+	# set Firewall to auto start on every system reboot
+	sudo systemctl enable --now firewalld
+
+	# set Firewall rules to allow HTTP & HTTPS traffic
+	sudo firewall-cmd --zone=public --add-port=80/tcp
+	sudo firewall-cmd --zone=public --permanent --add-port=80/tcp
+	sudo firewall-cmd --zone=public --add-port=443/tcp
+	sudo firewall-cmd --zone=public --permanent --add-port=443/tcp
+
+	# list Firewall rules, etc 
+	sudo firewall-cmd --list-all
+	sudo firewall-cmd --get-default-zone
+	sudo firewall-cmd --get-active-zones
+
+	# print info
+	echo "========================================================"
+	echo -e "${GREEN}[IMPORTANT]${NC} ${PURPLE}username:${NC} $USERID ${PURPLE}password:${NC} $PASSWORD ${PURPLE}port:${NC} $PORT \nplease note them down in your notebook. These values are non-recoverable"
 	exit 0;
+
 fi
 
